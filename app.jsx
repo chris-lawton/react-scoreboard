6a6fafe683453ae018efec6225413027d154d785
Chris Lawton <chris.lawton@gdsinternational.com>
2017-05-11 11:59:46 +0100

    moving score out of the props object and into the state object

--- app.jsx
+++ app.jsx
@@ -29,15 +29,19 @@ Header.propTypes = {
 };
 
 var Counter = React.createClass({
-  propTypes: {
-    score: React.PropTypes.number.isRequired
+  propTypes: {},
+
+  getInitialState: function() {
+    return {
+      score: 0,
+    }
   },
 
   render: function() {
     return(
       <div className="counter">
         <button className="counter-action decrement"> - </button>
-        <div className="counter-score"> {this.props.score} </div>
+        <div className="counter-score"> {this.state.score} </div>
         <button className="counter-action increment"> + </button>
       </div>
     );
@@ -52,7 +56,7 @@ function Player(props) {
       </div>
 
       <div className="player-score">
-        <Counter score={props.score} />
+        <Counter />
       </div>
     </div>
   );
